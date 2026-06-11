934738a09225038f7979824568ac0546b0ea0c56
ConnerKlingensmith <connerkling22@gmail.com>
2026-06-11 16:43:51 +0000

    Add s3_bucket module from private registry

--- main.tf
+++ main.tf
@@ -127,3 +127,10 @@ module "ec2_instances" {
     environment = "development"
   }
 }
+
+module "s3_bucket" {
+  source  = "app.terraform.io/policy-as-code-training/s3-bucket-ck/aws"
+  version = "1.0.0"
+
+  bucket_name = "conn-s3-bucket-11june"
+}
